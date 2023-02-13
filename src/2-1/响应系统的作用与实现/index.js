4f7b368e1503c1cf96249830b350ad5c8cb602b6
fangbw17 <bowen.fang17@gmail.com>
2023-02-13 14:33:54 +0800

    reactive

--- src/2-1/响应系统的作用与实现/index.js
+++ src/2-1/响应系统的作用与实现/index.js
@@ -54,13 +54,18 @@
         //         address: '珞喻路1037号'
         //     }
         // }
-        const data = { ok: true, text: "hello world" };
+        const data = { ok: true, text: "hello world", foo: 20, bar: 12 };
 
         // 声明变量，保存副作用函数（避免副作用函数的命名硬编码）
         let activeEffect
         // effect 栈
         const effectStack = []
-
+        // 定义一个任务队列
+        const jobQueue = new Set()
+        // 使用 Promise.resolve() 创建一个 promise 实例，用它将一个任务添加到微任务队列
+        const p = Promise.resolve()
+        // 一个标志代表是否正在刷新队列
+        let isFlushing = false
         // 容器
         const bucket = new WeakMap()
         // Proxy 对象
@@ -125,6 +130,57 @@
             // effectSet && effectSet.forEach(fn => fn())
         }
 
+        // 调度执行
+        function flushJob() {
+            // 如果队列正在刷新，则什么都不做
+            if (isFlushing) return
+            // 设置为 true，代表正在刷新
+            isFlushing = true
+            // 在微任务队列中刷新 jobQueue 队列
+            p.then(() => {
+                jobQueue.forEach(job => job())
+            }).finally(() => {
+                // 结束重置 isFlushing 状态
+                isFlushing = false
+            })
+        }
+
+        // 计算属性 computed
+        function computed(getter) {
+            // value 用来缓存上一次计算的值
+            let value
+            // dirty 标志，用来标识是否需要重新计算值，为 true 则意味着“脏”，需要计算
+            let dirty = true
+            // 把 getter 作为副作用函数，创建一个 lazy 的 effect
+            const effectFn = registerEffect(getter, {
+                lazy: true,
+                scheduler() {
+                    if (!dirty) {
+                        dirty = true
+                        // 当计算属性依赖的响应式数据变化时，手动调用 trigger 函数触发响应
+                        trigger(obj, 'value')
+                    }
+                }
+            })
+
+            const obj = {
+                // 当读取到 value 时才执行 effectFn
+                get value() {
+                    // 只有“脏”时才计算值，并将得到的值缓存到 value 中
+                    if (dirty) {
+                        value = effectFn()
+                        // 将 dirty 设置为 false，下一次访问直接使用缓存到 value 中的值
+                        dirty = false
+                        console.log('计算');
+                    }
+                    track(obj, 'value')
+                    return value
+                }
+            }
+
+            return obj
+        }
+
         // 注册副作用函数
         function registerEffect(fn, options = {}) {
             const effectFn = () => {
@@ -134,17 +190,24 @@
                 activeEffect = effectFn
                 // 调用副作用函数之前将当前副作用函数压入栈中
                 effectStack.push(effectFn)
-                fn()
+                // 将 fn 的执行结果存储到 res 中
+                const res = fn()
                 // 在当前副作用函数执行完毕后，将当前副作用函数弹出栈，并把 activeEffect 还原为之前的值
                 effectStack.pop()
                 activeEffect = effectStack[effectStack.length - 1]
+                // 将 res 作为 effectFn 的返回值
+                return res
             }
             // 挂载 options
             effectFn.options = options
             // activeEffect.deps 用来存储所有与该副作用函数相关联的依赖集合
             effectFn.deps = []
-            // 执行副作用函数
-            effectFn()
+            // 当不存在 lazy 的时候，才执行
+            if (!options.lazy) {
+                // 执行副作用函数
+                effectFn()
+            }
+            return effectFn
         }
         // 清除函数
         function cleanup(effectFn) {
@@ -160,14 +223,29 @@
         }
         // 执行注册
         registerEffect(() => {
-            document.body.innerHTML = obj.ok ? obj.text : 'no'
+            document.body.innerHTML = obj.foo
+            console.log(obj.foo);
+        }, {
+            scheduler(fn) {
+                // 每次调度时，将副作用函数添加到 jobQueue 队列中
+                jobQueue.add(fn)
+                // 调用 flushJob 刷新队列
+                flushJob()
+            }
         })
 
-        setTimeout(() => {
-            // obj.text = 'hello vue'
-            obj.ok = false
-            console.log(bucket);
-        }, 2000)
+        // obj.foo++
+        // obj.foo++
+        // setTimeout(() => {
+        //     obj.foo++
+        // }, 2000)
+
+        const sumRes = computed(() => obj.foo + obj.bar)
+        registerEffect(() => {
+            console.log(sumRes.value);
+        })
+        obj.foo++
+
     }
     fn()
 }
